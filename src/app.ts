1b3948aa4af3d1eb7645c98f45e09f8585e74f7d
Mikko Vuorinen <vuorinem@gmail.com>
2017-04-17 00:03:50 +0100

    Add routes for view and edit page

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
-export class App {
-  message = 'Hello World!';
+import { ConfiguresRouter, RouterConfiguration } from "aurelia-router";
+
+export class App implements ConfiguresRouter {
+  public configureRouter(config: RouterConfiguration) {
+    config.map([
+      {
+        route: "",
+        redirect: "create",
+      },
+      {
+        route: "create",
+        moduleId: "edit",
+      },
+      {
+        route: "edit/:name",
+        moduleId: "edit",
+      },
+      {
+        route: "view/:name",
+        moduleId: "view",
+      },
+    ]);
+  }
 }
