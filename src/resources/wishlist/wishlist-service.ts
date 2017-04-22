9e6197d344d13a49a51de5c6d37bbcb44651efe7
Mikko Vuorinen <vuorinem@gmail.com>
2017-04-22 20:53:42 +0100

    Include wishes in test wishlist

--- src/resources/wishlist/wishlist-service.ts
+++ src/resources/wishlist/wishlist-service.ts
@@ -1,10 +1,18 @@
+import { autoinject } from 'aurelia-framework';
 import { WishlistModel } from './wishlist-model';
+import { WishService } from '../wish/wish-service';
 
+@autoinject
 export class WishlistService {
+    constructor(private wishService: WishService) {
+    }
+
     public async get(name: string): Promise<WishlistModel> {
         let wishlist = new WishlistModel();
         wishlist.name = 'test';
         wishlist.title = 'Test Wishlist';
+
+        wishlist.wishes.push(...await this.wishService.getAll('test'));
 
         return await wishlist;
     }
