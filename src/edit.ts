b20d358af8cacc0706c3e248d169c3be7c6fd8b5
Mikko Vuorinen <vuorinem@gmail.com>
2017-04-22 19:52:35 +0100

    Load Wishlist when activating Edit route

--- src/edit.ts
+++ src/edit.ts
@@ -1,3 +1,22 @@
-export class Edit {
-    
+import { autoinject } from 'aurelia-framework';
+import { RoutableComponentActivate } from 'aurelia-router';
+import { WishlistService } from './resources/wishlist/wishlist-service';
+import { WishlistModel } from './resources/wishlist/wishlist-model';
+
+@autoinject
+export class Edit implements RoutableComponentActivate {
+
+    private wishlist: WishlistModel;
+
+    constructor(private wishlistService: WishlistService) {
+    }
+
+    public async activate(params: { name?: string }) {
+        if (params.name) {
+            this.wishlist = await this.wishlistService.get(params.name);
+        } else {
+            this.wishlist = new WishlistModel();
+        }
+    }
+
 }
