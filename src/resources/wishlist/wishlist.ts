c9ff3da4fbedeba92de727cf08a19d3914c16f1a
Mikko Vuorinen <vuorinem@gmail.com>
2017-04-23 19:33:18 +0100

    Remove wish from list when deleted

--- src/resources/wishlist/wishlist.ts
+++ src/resources/wishlist/wishlist.ts
@@ -1,9 +1,14 @@
 import { bindable } from 'aurelia-framework';
 import { WishlistModel } from './wishlist-model';
+import { WishModel } from '../wish/wish-model';
 
 export class Wishlist {
 
     @bindable private wishlist: WishlistModel;
     @bindable canEdit: boolean = false;
+
+    private deleteWish(wish: WishModel) {
+        this.wishlist.wishes.splice(this.wishlist.wishes.indexOf(wish), 1);
+    }
 
 }
