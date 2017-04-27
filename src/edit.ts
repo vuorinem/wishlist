c4b28e4b840fe14df6910ba5ded494620e9cf831
Mikko Vuorinen <vuorinem@gmail.com>
2017-04-27 22:39:36 +0100

    Load wishlist inside the component

--- src/edit.ts
+++ src/edit.ts
@@ -1,22 +1,13 @@
 import { autoinject } from 'aurelia-framework';
 import { RoutableComponentActivate } from 'aurelia-router';
-import { WishlistService } from './resources/wishlist/wishlist-service';
-import { WishlistModel } from './resources/wishlist/wishlist-model';
 
 @autoinject
 export class Edit implements RoutableComponentActivate {
 
-    private wishlist: WishlistModel;
-
-    constructor(private wishlistService: WishlistService) {
-    }
+    private name?: string;
 
     public async activate(params: { name?: string }) {
-        if (params.name) {
-            this.wishlist = await this.wishlistService.get(params.name);
-        } else {
-            this.wishlist = new WishlistModel();
-        }
+        this.name = params.name;
     }
 
 }
--- src/resources/wishlist/wishlist.ts
+++ src/resources/wishlist/wishlist.ts
@@ -1,11 +1,26 @@
-import { bindable, TaskQueue } from 'aurelia-framework';
+import { autoinject, bindable, TaskQueue } from 'aurelia-framework';
+import { WishlistService } from './wishlist-service';
 import { WishlistModel } from './wishlist-model';
 import { WishModel } from '../wish/wish-model';
 
+@autoinject
 export class Wishlist {
 
-    @bindable private wishlist: WishlistModel;
+    @bindable name: string;
     @bindable canEdit: boolean = false;
+
+    private wishlist: WishlistModel;
+
+    constructor(private wishlistService: WishlistService) {
+    }
+
+    public async nameChanged() {
+        if (this.name) {
+            this.wishlist = await this.wishlistService.get(this.name);
+        } else {
+            this.wishlist = new WishlistModel();
+        }
+    }
 
     private deleteWish(wish: WishModel) {
         this.wishlist.wishes.splice(this.wishlist.wishes.indexOf(wish), 1);
