43a25bfc44b3d64c3f610d9a94e283a999d523ad
Mikko Vuorinen <mikko.vuorinen@codify.ltd.uk>
2017-04-28 13:36:57 +0100

    Allow editing wishlist name Update url when changing name

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ export class App implements ConfiguresRouter {
         moduleId: 'edit',
       },
       {
+        name: 'edit',
         route: 'edit/:name',
         moduleId: 'edit',
       },
--- src/edit.ts
+++ src/edit.ts
@@ -1,13 +1,20 @@
-import { autoinject } from 'aurelia-framework';
-import { RoutableComponentActivate } from 'aurelia-router';
+import { autoinject, observable } from 'aurelia-framework';
+import { RoutableComponentActivate, Router } from 'aurelia-router';
 
 @autoinject
 export class Edit implements RoutableComponentActivate {
 
-    private name?: string;
+    @observable private name?: string;
+
+    constructor(private router: Router) {
+    }
 
     public async activate(params: { name?: string }) {
         this.name = params.name;
+    }
+
+    private nameChanged() {
+        this.router.navigateToRoute('edit', { name: this.name }, { trigger: false });
     }
 
 }
